6e4db1ff6b9c986c94d2ad803bd1cfb99237fbe5
IshitaSaxena29 <75059218+IshitaSaxena29@users.noreply.github.com>
2020-12-07 00:25:03 +0530

    Add files via upload

---
+++ PROGRAM 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n;int i;
+    printf("ENTER VALUE OF N :");
+    scanf("%d",&n);
+    printf("ALL NATURAL NUMBERS FROM 1 TO N ARE: \n");
+    for(i=1;i<=n;i++)
+    printf("%d ",i);
+    
+}
