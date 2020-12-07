13fa23cf5bda55069a73c014a82d5ee21a720183
IshitaSaxena29 <75059218+IshitaSaxena29@users.noreply.github.com>
2020-12-07 09:07:59 +0530

    Add files via upload

---
+++ PATTERN 3.c
@@ -0,0 +1,24 @@
+ #include <stdio.h>
+int main()
+{
+    int x = 0,y = 0;
+    unsigned int rows = 0;
+    printf("Enter the number of rows = ");
+    scanf("%u",&rows);
+    for(x=1; x<=rows; ++x)
+{
+    
+
+        for(y=1; y<=x; ++y)
+        {
+            printf(" ");
+        }
+        
+        for(y =1; y <=((rows*2)-((2*x)-1)); ++y)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
